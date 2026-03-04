0bd4a388224194f2ff2de0093317ba7baab8b13d
taco1999 <115353463+iamxiaobing47@users.noreply.github.com>
2026-03-05 00:46:57 +0800

    init

--- src/main/java/com/taco/backend_demo/BackendDemoApplication.java
+++ src/main/java/com/taco/backend_demo/BackendDemoApplication.java
@@ -1,5 +1,7 @@
 package com.taco.backend_demo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +28,19 @@ public class BackendDemoApplication {
                         .maxAge(3600);
             }
         };
+    }
+//
+//    访问 API 文档：应用启动后，您可以通过以下地址访问生成的 API 文档：
+//
+//    Swagger UI：http://localhost:8080/swagger-ui.html
+//    OpenAPI 规范：http://localhost:8080/v3/api-docs
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("后端演示 API")
+                        .version("1.0.0")
+                        .description("后端演示项目的 API 文档"));
     }
 
 }
--- src/main/java/com/taco/backend_demo/test/TestController.java
+++ src/main/java/com/taco/backend_demo/test/TestController.java
@@ -3,6 +3,8 @@ package com.taco.backend_demo.test;
 
 import com.taco.backend_demo.common.response.Response;
 import com.taco.backend_demo.common.response.ResponseFactory;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Tag(name = "测试接口", description = "测试相关的接口")
 public class TestController {
 
     @GetMapping
+    @Operation(summary = "测试接口", description = "返回测试字符串")
     public ResponseEntity<Response<String>> test() {
         return ResponseFactory.success("test");
     }
